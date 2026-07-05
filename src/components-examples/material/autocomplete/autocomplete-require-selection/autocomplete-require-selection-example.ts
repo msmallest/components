b4b78885dbff3c954f14120482529caf7ca7afa4
Michael Small <michael.small@relationshipone.com>
2026-07-05 14:10:55 -0500

    docs: convert `MatAutocomplete` to signals

--- src/components-examples/material/autocomplete/autocomplete-require-selection/autocomplete-require-selection-example.ts
+++ src/components-examples/material/autocomplete/autocomplete-require-selection/autocomplete-require-selection-example.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, ViewChild} from '@angular/core';
+import {Component, ElementRef, Signal, signal, ViewChild, WritableSignal} from '@angular/core';
 import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatInputModule} from '@angular/material/input';
@@ -23,10 +23,10 @@ export class AutocompleteRequireSelectionExample {
   @ViewChild('input') input!: ElementRef<HTMLInputElement>;
   myControl = new FormControl('');
   options: string[] = ['One', 'Two', 'Three', 'Four', 'Five'];
-  filteredOptions: string[] = this.options.slice();
+  filteredOptions: WritableSignal<string[]> = signal(this.options.slice());
 
   filter(): void {
     const filterValue = this.input.nativeElement.value.toLowerCase();
-    this.filteredOptions = this.options.filter(o => o.toLowerCase().includes(filterValue));
+    this.filteredOptions.set(this.options.filter(o => o.toLowerCase().includes(filterValue)));
   }
 }
